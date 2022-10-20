b0048446ef746a281ea99ab829b60ed782c2001b
HenryHung <hungwork510031@gmail.com>
2022-10-20 16:26:35 +0800

    update

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const task = express();
+const express = require("express")
+const task = express()
 
 //.env環境檔案
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require("dotenv")
+dotenv.config()
 //cors 跨來源資源共用
-const cors = require('cors');
-task.use(cors({
-    origin: 'process.env.ROUTER_CORS',
-    credentials: true,
-}))
+const cors = require("cors")
+task.use(
+    cors({
+        origin: process.env.ROUTER_CORS,
+        credentials: true,
+    })
+)
 //設定port 與 host
 const host = process.env.ROUTER_HOST
-const port = process.env.PORT || 3000
+const port = process.env.ROUTER_PORT || 3000
 //用於解析json row txt URL-encoded格式
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser")
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 //mongodb
-const mongoose = require('mongoose');
-const mongodbconfig = require('./database/mongodb')
+const mongoose = require("mongoose")
+const mongodbconfig = require("./database/mongodb")
 
 const mongoDbStatus = mongoose.connection
-mongoDbStatus.on('error', err => console.error('connection error', err))
-mongoDbStatus.once('open', (db) => console.log('Connection to mongodb'))
+mongoDbStatus.on("error", err => console.error("connection error", err))
+mongoDbStatus.once("open", db => console.log("Connection to mongodb"))
 
 //express helmet
-const helmet = require('helmet')
+const helmet = require("helmet")
 
 //開啟DNS預讀取
 task.use(helmet({ dnsPrefetchControl: { allow: true } }))
@@ -35,48 +37,51 @@ task.use(helmet({ dnsPrefetchControl: { allow: true } }))
 task.use(
     helmet.contentSecurityPolicy({
         directives: {
-            "script-src": ["self", "https://code.jquery.com", 
-            "https://cdn.jsdelivr.net",'http://localhost:3000',
-            'https://cdnjs.cloudflare.com'],
+            "script-src": [
+                "self",
+                "https://code.jquery.com",
+                "https://cdn.jsdelivr.net",
+                "https://cdnjs.cloudflare.com",
+            ],
         },
     })
-);
+)
 
 //passport
-const passport = require('passport')
-const session = require('express-session')
+const passport = require("passport")
+const session = require("express-session")
 //引入passport的Model
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser")
 task.use(cookieParser())
 //配置seesion passport
-task.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-}))
+task.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
 
 //初始化passport
 task.use(passport.initialize())
 task.use(passport.session())
-require('./database/passportjs')(passport)
-
+require("./database/passportjs")(passport)
 
 //Router
-const stageCheckRoutes = require('./router/stagecheckroutes')
-const dashboardRoutes = require('./router/dashboardroutes')
-const adminRoutes = require('./router/adminroutes')
-const studentRoutes = require('./router/studentroutes')
-
+const stageCheckRoutes = require("./router/stagecheckroutes")
+const dashboardRoutes = require("./router/dashboardroutes")
+const adminRoutes = require("./router/adminroutes")
+const studentRoutes = require("./router/studentroutes")
 
 task.use(express.json())
 //靜態物件取得從public
-task.use(express.static('public'))
-const path = require('path')
-task.use(express.static(path.join(__dirname, 'public')))
+task.use(express.static("public"))
+const path = require("path")
+task.use(express.static(path.join(__dirname, "public")))
 task.set("view engine", "pug")
 
-task.use(bodyParser.json());
-task.use(urlencodedParser);
+task.use(bodyParser.json())
+task.use(urlencodedParser)
 // task.use(
 //     cors({
 //         origin:process.env.CORS_ROUTER,
@@ -104,15 +109,14 @@ task.use(urlencodedParser);
 task.post(process.env.ROUTER_MAIN_LOGIN, (req, res, next) => {
     passport.authenticate("local", (err, user, info) => {
         if (err) {
-            res.send('/404')
+            res.send("/404")
             throw err
         }
         if (!user) {
             console.log(info.message)
             res.send(`${info.message}`)
-        }
-        else {
-            req.logIn(user, (err) => {
+        } else {
+            req.logIn(user, err => {
                 if (err) throw err
                 res.send(`/dashboard/${req.body.username}`)
             })
@@ -122,12 +126,11 @@ task.post(process.env.ROUTER_MAIN_LOGIN, (req, res, next) => {
 
 //登出
 task.get(process.env.ROUTER_MAIN_LOGOUT, (req, res, next) => {
-    req.logout((err) => {
+    req.logout(err => {
         if (err) return next(err)
-        res.send('/')
+        res.send("/")
     })
 })
-
 
 //守門員用法use
 // task.use((req, res, next) => {
@@ -135,32 +138,36 @@ task.get(process.env.ROUTER_MAIN_LOGOUT, (req, res, next) => {
 // })
 let isAuthenticated = function (req, res, next) {
     if (req.isAuthenticated()) return next()
-    res.redirect('/')
+    res.redirect("/")
 }
-task.get('/', (req, res) => {
+task.get("/", (req, res) => {
     if (req.user !== undefined) {
         res.redirect(`/dashboard/${req.user.studentId}`)
     } else {
-        res.render('index')
+        res.render("index")
     }
 })
 
 //Stagepage
 task.use(process.env.ROUTER_MAIN_DASHBOARD, isAuthenticated, dashboardRoutes)
 //stageCheck
-task.use(process.env.ROUTER_MAIN_STUDENTSTAGE, isAuthenticated, stageCheckRoutes);
+task.use(
+    process.env.ROUTER_MAIN_STUDENTSTAGE,
+    isAuthenticated,
+    stageCheckRoutes
+)
 //student
 task.use(process.env.ROUTER_MAIN_STUDENT, isAuthenticated, studentRoutes)
 //admin
-task.use(process.env.ROUTER_MAIN_ADMIN, adminRoutes);
+task.use(process.env.ROUTER_MAIN_ADMIN, adminRoutes)
 
-task.get('/checkdata/:filename', async (req, res) => {
+task.get("/checkdata/:filename", async (req, res) => {
     res.sendFile(`${__dirname}/public/media/pdf/${req.params.filename}`)
 })
 
 //無此路由
 task.use((req, res, next) => {
-    res.status(404).send("找不到");
+    res.status(404).send("找不到")
 })
 
 //程式錯誤
@@ -169,6 +176,6 @@ task.use((err, req, res, next) => {
     res.status(500).send("程式有問題 請檢查")
 })
 
-
-
-task.listen(port, () => { console.log("Server is runing at " + host + " : " + port) })
+task.listen(port, () => {
+    console.log("Server is runing at " + host + " : " + port)
+})
